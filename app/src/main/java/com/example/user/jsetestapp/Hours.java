bb73fa12f62acc92548252502b6988b723f29c3e
rsorotzkin <rsorotzkin@gmail.com>
2015-10-20 11:35:11 -0400

    commit

--- app/src/main/java/com/example/user/jsetestapp/Hours.java
+++ app/src/main/java/com/example/user/jsetestapp/Hours.java
@@ -3,13 +3,78 @@ package com.example.user.jsetestapp;
 import org.joda.time.DateTime;
 
 public class Hours {
-    public static enum DayOfWeek{SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY};
+    public static enum DayOfWeek {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY};
 
+    String name;
     DayOfWeek dayOfWeek;
-    DateTime startTime, endTime;
+    //DateTime startTime, endTime;
+    String startTime, endTime;
+    //String dayOfWeek;
 
-
-    public Hours(){
+    public Hours() {
 
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+//
+//    public DateTime getStartTime() {
+//        return startTime;
+//    }
+//
+//    public void setStartTime(DateTime startTime) {
+//        this.startTime = startTime;
+//    }
+//
+//    public DateTime getEndTime() {
+//        return endTime;
+//    }
+//
+//    public void setEndTime(DateTime endTime) {
+//        this.endTime = endTime;
+//    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        try {
+            this.dayOfWeek = DayOfWeek.valueOf(dayOfWeek);
+        } catch (Exception ex) {
+            this.dayOfWeek = DayOfWeek.SUNDAY;
+        }
+    }
+
+public String getStartTime() {
+    return startTime;
+}
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+//    public String getDayOfWeek() {
+//        return dayOfWeek;
+//    }
+//    public void setDayOfWeek(String dayOfWeek) {
+//        this.dayOfWeek = dayOfWeek;
+//    }
 }
--- app/src/main/java/com/example/user/jsetestapp/QueryMethods.java
+++ app/src/main/java/com/example/user/jsetestapp/QueryMethods.java
@@ -10,6 +10,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class QueryMethods extends Activity {
 
@@ -20,6 +21,10 @@ public class QueryMethods extends Activity {
 
     // URL to get testsJsonArray JSON
     private static String tests_url = "http://phpstack-1830-4794-62139.cloudwaysapps.com/tests.php";
+
+    // URL to get hoursJsonArray JSON
+    private static String hours_url = "http://phpstack-1830-4794-62139.cloudwaysapps.com/library_hours.php";
+
 
     // JSON Node names - locations
     private static final String TAG_LOCATIONS = "branches";
@@ -34,11 +39,19 @@ public class QueryMethods extends Activity {
     private static final String TAG_CLOSING_TIME = "closingTime";
     private static final String TAG_GENDER = "gender";
 
+    // JSON Node names - hours
+    private static final String TAG_HOURS = "hours";
+    private static final String TAG_LIBRARY_LOCATION = "name";
+    private static final String TAG_DAY_OF_WEEK= "dayOfWeek";
+    private static final String TAG_OPENING_TIME= "openingTime";
+    private static final String TAG_DURATION= "duration";
+
     // locationsJsonArray JSONArray
     JSONArray locationsJsonArray = null;
     // testsJsonArray JSONArray
     JSONArray testsJsonArray = null;
-
+    // hoursJsonArray JSONArray
+    JSONArray hoursJsonArray = null;
 
     String pid;
 
@@ -66,6 +79,24 @@ public class QueryMethods extends Activity {
     }
 
 
+    public ArrayList<HoursDataObject> getHoursArrayList() {
+
+        mainActivity.hoursFilteredArrayList = new ArrayList<HoursDataObject>();
+
+        for (Hours hours : mainActivity.hoursArrayList) {
+
+            HoursDataObject obj = new HoursDataObject(hours.getDayOfWeek().toString(),
+
+                    hours.getStartTime().toString(),
+                    hours.getEndTime().toString());
+
+            mainActivity.hoursFilteredArrayList.add(obj);
+        }
+
+        return mainActivity.hoursFilteredArrayList;
+    }
+
+
     public void setUpLocationsArrayList() {
 
         mainActivity.locationsArrayList = new ArrayList<String>();
@@ -85,6 +116,14 @@ public class QueryMethods extends Activity {
         new GetTests().execute();
     }
 
+    public void setUpHoursArrayList() {
+
+        mainActivity.hoursFilteredArrayList = new ArrayList<HoursDataObject>();
+        mainActivity.hoursArrayList = new ArrayList<Hours>();
+
+        // Calling async task to get json
+        new GetHours().execute();
+    }
 
     public void setMainActivity(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
@@ -205,6 +244,73 @@ public class QueryMethods extends Activity {
                 }
             } else {
                 Log.e("ServiceHandler", "Couldn't get any data from the tests_url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+        }
+
+    }
+
+
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetHours extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+//            // Showing progress dialog
+//            pDialog = new ProgressDialog(mainActivity.getApplicationContext());
+//            pDialog.setMessage("Please wait...");
+//            pDialog.setCancelable(false);
+//            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to locations_url and getting response
+            String jsonStr = sh.makeServiceCall(hours_url, ServiceHandler.GET);
+
+            Log.d("Response Hours: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    hoursJsonArray = jsonObj.getJSONArray(TAG_HOURS);
+
+                    // looping through All Tests
+                    for (int i = 0; i < hoursJsonArray.length(); i++) {
+
+                        JSONObject c = hoursJsonArray.getJSONObject(i);
+
+                        Hours hours = new Hours();
+                        hours.name = c.getString(TAG_LIBRARY_LOCATION);
+                        //hours.dayOfWeek = c.getString(TAG_DAY_OF_WEEK);
+                        hours.setDayOfWeek(c.getString(TAG_DAY_OF_WEEK));
+                        hours.startTime = c.getString(TAG_OPENING_TIME);
+                        hours.endTime = c.getString(TAG_DURATION);
+
+                        mainActivity.hoursArrayList.add(hours);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the hours_url");
             }
 
             return null;
