12f347878186467e3e7a05ecfef5280c5c66a840
rsorotzkin <rsorotzkin@gmail.com>
2015-10-21 15:17:15 -0400

    commit

--- app/src/main/java/com/example/user/jsetestapp/MyDialogFragment.java
+++ app/src/main/java/com/example/user/jsetestapp/MyDialogFragment.java
@@ -13,35 +13,51 @@ import android.support.v7.widget.ThemedSpinnerAdapter;
  * Created by Rochel on 10/9/2015.
  */
 public class MyDialogFragment extends android.app.DialogFragment {
-
+MainActivity mainActivity;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Bundle bundle = this.getArguments();
 
         String title = getArguments().getString("title");
         String message = getArguments().getString("message");
-        String number = getArguments().getString("number");
+        String positiveButton = getArguments().getString("positiveButton");
+        String negativeButton = getArguments().getString("negativeButton");
+        int icon = getArguments().getInt("icon");
+        final String TAG_LISTENER = getArguments().getString("tag_listener");
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(title);
         builder.setMessage(message);
-
-        builder.setPositiveButton("Call", new DialogInterface.OnClickListener() {
+        builder.setIcon(icon);
+        builder.setPositiveButton(positiveButton, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //dismiss();
-                String number = getArguments().getString("number");
-                Uri call = Uri.parse("tel:" + number);
-                Intent intent = new Intent(Intent.ACTION_CALL, call);
-                startActivity(intent);
+
+                 //((MainActivity)getActivity()).helperMethods.positiveButtonOnClickListener(TAG_LISTENER);
+
+            }
+
+        });
+
+        builder.setNegativeButton(negativeButton, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               // dismiss();
+               //((MainActivity)getActivity()).helperMethods.negativeButtonOnClickListener(TAG_LISTENER);
             }
         });
 
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dismiss();
-            }
-        });
+//            builder.setThirdButton(positiveButton, new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//
+//                }
+
         return builder.create();
     }
+    public void setMainActivity(MainActivity mainActivity) {
+
+        this.mainActivity = mainActivity;
+    }
+
 }
